0d7be9f1dac14a764eb11bf7222b0cccf68a497b
Nico Dieckmann <nico.dieckmann@gmx.de>
2024-12-19 19:23:15 +0100

    Hinzufügen einer Beschreibung für die main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
+"""
+main.py
 
+Dies ist der Einstiegspunkt der Anwendung. 
+Die GUI wird über die Klasse `ApplicationWindow` gestartet.
+
+Funktionen:
+- `main()`: Initialisiert die GUI und startet die Anwendung.
+
+Abhängigkeiten:
+- PyQt5
+- application_window aus dem `gui`-Modul
+"""
+
+
+def main() -> None:
+    """
+    
+    
+    """
+
+if __name__ == '__main__':
+    main()
